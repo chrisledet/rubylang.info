62d16d7d8daaf598178d3869a8a6e3f9844c93ab
Chris Ledet <chris@ledet.io>
2011-08-05 11:52:06 -0400

    new design!

---
+++ javascripts/app.js
@@ -0,0 +1,17 @@
+$('#download').live('click', function(e){
+  // alert('Downloading...');
+  
+  var WIN_INSTALLER = 'http://rubyinstaller.org/downloads/';
+  var SRC = 'http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.gz';
+  
+  if (navigator.userAgent.indexOf("Mac") != -1) {
+    window.location = SRC;
+  }
+  else if (navigator.userAgent.indexOf("Win") != -1) {
+    window.location = 'http://rubyinstaller.org/downloads/';
+  }
+  else if (navigator.userAgent.indexOf("Linux") != -1) {
+    window.location = SRC;
+  }
+  
+});
